f7bedd4f0a7fc4c85aa5154ac80b3cf997877789
nattapong18-en <664110310060@npu.ac.th>
2026-07-15 08:21:16 +0700

    do preramit

--- main.cpp
+++ main.cpp
@@ -5,14 +5,18 @@ using namespace std;
 void printTriangle(int n) {
 
   for (int i = 1; i <= n; ++i) {
-
-    for (int j = 0; j < (n - i); ++j) {
+    for (int j = 0; j < (n - i); ++j)
       cout << " ";
-    }
-
-    for (int k = 0; k < (2 * i - 1); ++k) {
+    for (int k = 0; k < (2 * i - 1); ++k)
       cout << "*";
-    }
+    cout << "\n";
+  }
+
+  for (int i = 1; i < n; ++i) {
+    for (int j = 0; j < i; ++j)
+      cout << " ";
+    for (int k = 0; k < (2 * (n - i) - 1); ++k)
+      cout << "*";
     cout << "\n";
   }
 }
